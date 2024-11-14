45e40ceabfa2f7f37b0c1889bdc47c0b562bddce
andha8664 <anne.d.ha@sjsu.edu>
2024-11-13 20:29:28 -0800

    Update model_sql.sql

--- dbt_airflow/dbt/models/model_sql.sql
+++ dbt_airflow/dbt/models/model_sql.sql
@@ -1,30 +1,25 @@
--- models/lab1/market_data.sql
-{{ config(
-    materialized='table',
-    tags=['lab1']
-) }}
-
-SELECT 
-    stock,
-    open,
-    high,
-    low,
-    close,
-    volume,
-    date
-FROM {{ ref('raw_market_data') }}
-
 -- models/forecast/prep_training.sql
 {{ config(
     materialized='table',
     tags=['prep_for_training']
 ) }}
 
+WITH market_data AS (
+    SELECT 
+        stock,
+        date,
+        close
+    FROM {{ ref('market_data') }}
+)
 SELECT 
     stock,
     date,
     close,
     LAG(close, 1) OVER (PARTITION BY stock ORDER BY date) as prev_day_close,
-    LAG(close, 7) OVER (PARTITION BY stock ORDER BY date) as prev_week_close
-FROM {{ ref('market_data') }}
-WHERE date >= DATEADD(day, -90, CURRENT_DATE())
+    LAG(close, 7) OVER (PARTITION BY stock ORDER BY date) as prev_week_close,
+    CASE 
+        WHEN COUNT(*) = 0 THEN NULL -- Handle scenarios with no data
+        ELSE COUNT(*) OVER () -- Optional: provide total record count
+    END AS total_records
+FROM market_data
+WHERE date >= DATEADD(day, -90, CURRENT_DATE());
